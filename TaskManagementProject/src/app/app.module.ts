b3144cde3aabff00593b0d1a985a09405110cd68
Marco Antonio <cootoonhoo@gmail.com>
2023-01-16 23:39:42 -0300

    Fazendo a MainPage

--- TaskManagementProject/src/app/app.module.ts
+++ TaskManagementProject/src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { StyledButtonComponent } from "./styled-button/styled-button.component";
+import { CustomHeaderComponent } from './custom-header/custom-header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomePageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomePageComponent,
+        StyledButtonComponent,
+        CustomHeaderComponent,
+
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ]
 })
 export class AppModule { }
