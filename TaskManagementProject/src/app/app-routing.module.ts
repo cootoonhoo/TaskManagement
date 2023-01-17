03b8fd7c69388242cc444b4f3b5b29d47602efb0
Marco Antonio <cootoonhoo@gmail.com>
2023-01-17 15:47:24 -0300

    Criando as rotas

--- TaskManagementProject/src/app/app-routing.module.ts
+++ TaskManagementProject/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes: Routes = [
+  {path:"", component: HomePageComponent},
+  {path:"login", component: LoginComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TaskManagementProject/src/app/app.module.ts
+++ TaskManagementProject/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { StyledButtonComponent } from "./styled-button/styled-button.component";
 import { CustomHeaderComponent } from './custom-header/custom-header.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
     declarations: [
@@ -13,6 +14,7 @@ import { CustomHeaderComponent } from './custom-header/custom-header.component';
         HomePageComponent,
         StyledButtonComponent,
         CustomHeaderComponent,
+        LoginComponent,
 
     ],
     providers: [],
